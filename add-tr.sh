90ac27581a450e7dae9cf82c23e0363f474fff2e
adamtech <adamtech323@gmail.com>
2022-02-16 22:13:05 +0800

    vps

--- add-tr.sh
+++ add-tr.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-echo "Checking VPS"
-red='\e[1;31m'
-green='\e[0;32m'
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/nikstore/acceptip/main/special-ip | grep $MYIP )
+MYIP=$(wget -qO- icanhazip.com);
+echo -e  "${RED}Checking VPS${NC}"
+sleep 2
+IZIN=$( curl https://raw.githubusercontent.com/nikstore/acceptip/main/acceptip | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
+echo -e "${GREEN}Permission Accepted...${NC}"
+sleep 2
+else
 clear
 echo -e ""
-else
-echo "pm @naimstore untuk daftar ip"
+echo -e "======================================="
+echo -e ""
+echo -e "${RED}Permission Denied...!!! ${NC}"
+echo -e "IP VPS ANDA BELUM TERDAFTAR"
+echo -e "Contact TELE @naimstore"
+echo -e "For Registration IP VPS"
+echo -e ""
+echo -e "======================================="
+echo -e ""
+rm setup.sh
 exit 0
 fi
 clear
