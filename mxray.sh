fa2b42827aadaafcdacda13f4046761b882cbda1
adamtech <adamtech323@gmail.com>
2022-02-16 23:22:34 +0800

    vps

--- mxray.sh
+++ mxray.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo -e  "${RED}Checking VPS${NC}"
-sleep 2
-IZIN=$( curl https://raw.githubusercontent.com/nikstore/acceptip/main/acceptip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${GREEN}Permission Accepted...${NC}"
-sleep 2
-else
-clear
-echo -e ""
-echo -e "======================================="
-echo -e ""
-echo -e "${RED}Permission Denied...!!! ${NC}"
-echo -e "IP VPS ANDA BELUM TERDAFTAR"
-echo -e "Contact TELE @naimstore"
-echo -e "For Registration IP VPS"
-echo -e ""
-echo -e "======================================="
-echo -e ""
-rm setup.sh
-exit 0
-fi
+
 
 clear
 
